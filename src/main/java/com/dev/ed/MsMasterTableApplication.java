315bc5818d9a13d90c8018f4ba503094eff5e7c1
dev.ed <edwar.moran.12@gmail.com>
2024-02-01 16:46:09 -0500

    config client

--- src/main/java/com/dev/ed/MsMasterTableApplication.java
+++ src/main/java/com/dev/ed/MsMasterTableApplication.java
@@ -1,9 +1,14 @@
 package com.dev.ed;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
+@EnableFeignClients("com.dev.ed")
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class MsMasterTableApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/dev/ed/infrastructure/helper/response/ReniecResponseHelper.java
@@ -0,0 +1,19 @@
+package com.dev.ed.infrastructure.helper.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReniecResponseHelper implements Serializable {
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private String digitoVerificador;
+}
