21f590cedd507327ca219e8e6f8419eaab39af2b
dev.ed <edwar.moran.12@gmail.com>
2024-02-03 15:28:24 -0500

    validation modify

--- src/main/java/com/dev/ed/domain/model/request/RequestCaptures.java
+++ src/main/java/com/dev/ed/domain/model/request/RequestCaptures.java
@@ -1,6 +1,5 @@
 package com.dev.ed.domain.model.request;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,15 +15,12 @@ import java.util.Set;
 @NoArgsConstructor
 public class RequestCaptures {
 
-    @NotEmpty(message = "Debes ingresar el fecha")
     @NotNull(message = "No debe ser nulo el campo date")
     private LocalDate dateCapture;
 
-    @NotEmpty(message = "Debes ingresar el codigo del cliente")
     @NotNull(message = "No debe ser nulo el campo customerId")
     private Long customerId;
 
-    @NotEmpty(message = "Debes ingresar el codigo del vendedor")
     @NotNull(message = "No debe ser nulo el campo sellerId")
     private Long sellerId;
 
